86290d03b0641e437fdc1d2aaf0442ff1b246282
The Herald Sun <samuelryanmason@gmail.com>
2025-08-17 17:50:51 +1000

    Hyprshader crap

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_python_lsp = "pyright"
+
+vim.o.tabstop = 4 -- Sets the visual width of a tab character
+vim.o.expandtab = true -- Converts tabs to spaces when you press Tab
+vim.o.softtabstop = 4 -- Number of spaces a Tab or Backspace in insert mode counts for
+vim.o.shiftwidth = 4 -- Number of spaces used for auto-indentation and indent commands
