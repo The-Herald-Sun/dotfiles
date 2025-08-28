a95db3f008611652f9b74172d4646b9ad3bee8bd
The Herald Sun <samuelryanmason@gmail.com>
2025-08-28 16:35:04 +1000

    Refactored colorshceme config and switched to Shatur/newvim-ayu

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -4,7 +4,7 @@
 vim.g.lazyvim_python_lsp = "pyright"
 -- then you need to set the option below.
 
-vim.o.tabstop = 4 -- Sets the visual width of a tab character
-vim.o.expandtab = true -- Converts tabs to spaces when you press Tab
-vim.o.softtabstop = 4 -- Number of spaces a Tab or Backspace in insert mode counts for
-vim.o.shiftwidth = 4 -- Number of spaces used for auto-indentation and indent commands
+-- vim.o.tabstop = 4 -- Sets the visual width of a tab character
+-- vim.o.expandtab = true -- Converts tabs to spaces when you press Tab
+-- vim.o.softtabstop = 4 -- Number of spaces a Tab or Backspace in insert mode counts for
+-- vim.o.shiftwidth = 4 -- Number of spaces used for auto-indentation and indent commands
--- nvim/.config/nvim/lua/plugins/ayu.lua
+++ nvim/.config/nvim/lua/plugins/ayu.lua
@@ -1,21 +1,23 @@
--- ~/.config/nvim/lua/plugins/ayu.lua
 return {
   {
-    "ayu-theme/ayu-vim",
-    name = "ayu",
+    "Shatur/neovim-ayu",
     lazy = false,
     priority = 1000,
     config = function()
-      -- Initial Ayu setup
-      vim.g.ayucolor = "mirage"
-      vim.g.ayu_transparent_sidebar = 1
-      vim.g.ayu_disable_background = 1
-
-      vim.opt.winblend = 0
-      vim.opt.termguicolors = true
-
-      -- Apply the colorscheme. This is crucial for initializing the plugin.
-      vim.cmd("colorscheme ayu")
+      require("ayu").setup({
+        mirage = true,
+        overrides = {
+          Normal = { bg = "None" },
+          NormalFloat = { bg = "none" },
+          ColorColumn = { bg = "None" },
+          SignColumn = { bg = "None" },
+          Folded = { bg = "None" },
+          FoldColumn = { bg = "None" },
+          CursorLine = { bg = "None" },
+          CursorColumn = { bg = "None" },
+          VertSplit = { bg = "None" },
+        },
+      })
     end,
   },
 }
